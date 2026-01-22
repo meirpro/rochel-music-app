38764353c9bf90ea78618707cd71ffece1389437
Light-Wing on MacBook 14 <23284632+meirpro@users.noreply.github.com>
2026-01-22 05:00:37 -0500

    Remove unused legacy components

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { SheetMusic } from "./SheetMusic";
-export { SongSelector } from "./SongSelector";
-export { PlaybackControls } from "./PlaybackControls";
-export { DisplayOptions } from "./DisplayOptions";
-export { ColorLegend } from "./ColorLegend";
-export { NoteEditor } from "./NoteEditor";
-export type { EditorNote, NoteTool, RepeatMarker } from "./NoteEditor";
